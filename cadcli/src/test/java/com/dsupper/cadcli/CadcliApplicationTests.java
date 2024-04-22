6a843e9101fed4381cf7bbb79919a42ab9c87a7c
Eugênio <eugenio@sisger.com.br>
2024-04-22 19:44:27 -0300

    Criado o projeto CadCli

---
+++ cadcli/src/test/java/com/dsupper/cadcli/CadcliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsupper.cadcli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadcliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
